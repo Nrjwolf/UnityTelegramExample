587268da4d270a0ed6cade072d29b9847813c244
Alexander <Nrjwolf@MacBook-Pro-Alexander.local>
2017-11-26 14:08:56 +0300

    init

---
+++ Assets/Main.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class View
+    {
+        [Header("Buttons")]
+        public Button getMe;
+        public Button sendDocument;
+        public Button sendPhoto;
+        public Button sendMessage;
+
+        [Space(10)]
+        [Header("Text")]
+        public Text inputText;
+    }
+
+    public Telegram telegram;
+    public View view;
+
+    void Start()
+    {
+        // Bot info
+        view.getMe.onClick.AddListener(() => telegram.GetMe());
+
+        // Send any document
+        view.sendDocument.onClick.AddListener(() =>
+        {
+			// send txt file
+            var bytes = System.Text.Encoding.UTF8.GetBytes(GetUserInfo());
+            telegram.SendFile(bytes, "file.txt", GetUserInfo());
+        });
+
+        // send file as photo
+        view.sendPhoto.onClick.AddListener(() =>
+        {
+            var screenshotName = "Screenshot.png";
+            ScreenCapture.CaptureScreenshot("Assets/" + screenshotName);
+            var bytes = File.ReadAllBytes(Application.dataPath + "/" + screenshotName);
+            telegram.SendPhoto(bytes, screenshotName);
+        });
+
+        // send message
+        view.sendMessage.onClick.AddListener(() =>
+        {
+            telegram.SendMessage(view.inputText.text);
+        });
+    }
+
+    /**
+	* Utils
+	*
+	*/
+
+    private string GetUserInfo()
+    {
+        return string.Format("From :\n{0}\n{1}", SystemInfo.deviceName, SystemInfo.deviceModel);
+    }
+}
---
+++ Assets/Telegram.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Telegram : MonoBehaviour
+{
+    public string chat_id = "0000000"; // ID (you can know your id via @userinfobot)
+    public string TOKEN = "00000:aaaaaa"; // bot token (@BotFather)
+    public string API_URL
+    {
+        get
+        {
+            return string.Format("https://api.telegram.org/bot{0}/", TOKEN);
+        }
+    }
+
+    public void GetMe()
+    {
+        WWWForm form = new WWWForm();
+        UnityWebRequest www = UnityWebRequest.Post(API_URL + "getMe", form);
+        StartCoroutine(SendRequest(www));
+    }
+
+    public void SendFile(byte[] bytes, string filename, string caption = "")
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("chat_id", chat_id);
+        form.AddField("caption", caption);
+        form.AddBinaryData("document", bytes, filename, "filename");
+        UnityWebRequest www = UnityWebRequest.Post(API_URL + "sendDocument?", form);
+        StartCoroutine(SendRequest(www));
+    }
+
+    public void SendPhoto(byte[] bytes, string filename, string caption = "")
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("chat_id", chat_id);
+        form.AddField("caption", caption);
+        form.AddBinaryData("photo", bytes, filename, "filename");
+        UnityWebRequest www = UnityWebRequest.Post(API_URL + "sendPhoto?", form);
+        StartCoroutine(SendRequest(www));
+    }
+
+    public new void SendMessage(string text)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("chat_id", chat_id);
+        form.AddField("text", text);
+        UnityWebRequest www = UnityWebRequest.Post(API_URL + "sendMessage?", form);
+        StartCoroutine(SendRequest(www));
+    }
+
+    IEnumerator SendRequest(UnityWebRequest www)
+    {
+        yield return www.SendWebRequest();
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            var w = www;
+            Debug.Log("Success!\n" + www.downloadHandler.text);
+        }
+    }
+
+
+}
